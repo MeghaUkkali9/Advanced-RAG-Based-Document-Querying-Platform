186f96a8d8af94d39b7643811393fee50d62ba66
MeghaUkkali9 <meghaukkali99@gmail.com>
2026-03-21 20:32:12 +0530

    remove main

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -2,7 +2,9 @@ import sys
 import traceback
 
 class DocumentQueryingPortalException(Exception):
-    """Custom Exception for Document querying portal"""
+    """
+    Custom Exception for Document querying portal
+    """
     def __init__(self, err_msg:str, err_details:sys):
         _,_,exc_information = err_details.exc_info()
         self.file_name = exc_information.tb_frame.f_code.co_filename
@@ -13,9 +15,9 @@ class DocumentQueryingPortalException(Exception):
     def __str__(self):
        return f""" Error Occrued in {self.file_name} at line [{self.line_no}] Message: {self.error_message}, traceback: {self.traceback_str}"""
 
-if __name__ == "__main__":
-    try:
-        a  = 1/0
-        print(a)
-    except Exception as e:
-        raise DocumentQueryingPortalException(e, sys) 
+# if __name__ == "__main__":
+#     try:
+#         a  = 1/0
+#         print(a)
+#     except Exception as e:
+#         raise DocumentQueryingPortalException(e, sys) 
