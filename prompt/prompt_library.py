157980a8d592e8d64cbbcd4da3ccf0fd9dc13dac
MeghaUkkali9 <meghaukkali99@gmail.com>
2026-03-25 20:39:43 +0530

    Adding model and promt for document caomparison

--- prompt/prompt_library.py
+++ prompt/prompt_library.py
@@ -8,3 +8,18 @@ Return ONLY valid JSON matching schema below.
 Analyse this Document:
 {document_text}
 """)
+
+document_comparison_prompt = ChatPromptTemplate.from_template("""
+You are a helpful assistant trained to compare two documents and summarize the differences.
+Return ONLY valid JSON matching schema below.
+{format_instructions}
+
+Compare these Documents:
+Document 1: {document_1_text}
+Document 2: {document_2_text}
+""")
+
+PROMPT_REGISTRY={
+    "document_analysis": prompt,
+    "document_comparison": document_comparison_prompt
+}
