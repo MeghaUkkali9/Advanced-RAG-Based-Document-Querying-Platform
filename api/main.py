14568339fc4cd87f50a617f645892a9f7e8c0331
MeghaUkkali9 <meghaukkali99@gmail.com>
2026-07-27 05:37:34 +0530

    fix invalid cors config, wildcard origin cant be paired with credentials

--- api/main.py
+++ api/main.py
@@ -27,9 +27,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
-    allow_headers=["*"] 
+    allow_headers=["*"]
 )
 
 FAISS_BASE = os.getenv("FAISS_BASE", "faiss_index")
